fceea48f711a6b9417cdd0516244b69162581fa1
Shahjada Talukdar <shahjada.talukdar@gmail.com>
2017-05-29 00:40:51 +0200

    Basic App

---
+++ Components/App.jsx
@@ -0,0 +1,14 @@
+"use strict";
+
+import React from 'react';
+
+export default class App extends React.Component {
+  render() {
+    return(
+      <div>
+        <h1>React Basic Starter</h1>
+      </div>
+    )
+  }
+};
+
